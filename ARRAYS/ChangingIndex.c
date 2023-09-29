7bdc588ebfc87f053103cadce1728bd2806b47e4
Aditya Raj <adityasahil24@gmail.com>
2023-09-29 23:54:46 +0530

    completed the code.
    In this commit, I have completed this task. I added user input function in this to make it user - friendly.

--- ARRAYS/ChangingIndex.c
+++ ARRAYS/ChangingIndex.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
-void changeIndex(int arr[]){
-  for(int i = 0; i <= 
+int main()
+{
+    int n;
+    printf("Enter the number of indices:: ");
+    scanf("%d", &n);
+    int arr[n];  //={5, 7, 8, 9, 46, 12, 13, 14, 21, 23};
+    printf("Enter elements of the array:: ");
+    for(int i = 0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    for(int i = 0; i < n; i++)
+    {
+        if(i % 2 == 0) arr[i] = arr[i] + 10;
+        else arr[i] = arr[i] * 2;
+        printf("%d\t", arr[i]);
+    }
+    return 0;    
 }
