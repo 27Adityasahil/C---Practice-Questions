0e328478b6b34763bd35b7aef00765c22f2402c0
Aditya Raj <adityasahil24@gmail.com>
2023-11-05 12:47:57 +0530

    n X n matrix transpose

---
+++ ARRAYS/nXnMatrixTranspose.c
@@ -0,0 +1,42 @@
+//Write a program to change the given n x n matrix to it's transpose (same array will be changed, not to use any extra array)
+
+#include <stdio.h>
+
+int main(){
+
+    int n;
+    printf("Enter the rows and column of your 'n x n' matrix to find it's transpose:: ");
+    scanf("%d", &n);
+
+    int arr[n][n];
+
+    printf("Enter the elements of your %d x %d matrix:: \n", n, n);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){ // loop for printing the original array for referrence
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){ // loop to transpose the given matrix by replacing the rows indices to column and elements too
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){ // printing the transposed array stored in the same matrix
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
